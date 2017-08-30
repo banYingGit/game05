dbe022c3bf9eab9f32bb065252d0f0dbca1337f4
[banYingGit] <[635221846@qq.com]>
2017-08-30 14:21:25 +0800

    游戏项目提交

--- javascript/gameView.js
+++ javascript/gameView.js
@@ -4,10 +4,22 @@
 /* 全局变量
  * clickIndex：点击索引
  * clickN：点击次数
+ * atuoTime：倒计时
+ * errorNum ：错误点击次数
+ * maxNum：正确点击最大值
+ * sysTimeN：游戏开始时间
+ * sysTimeO：游戏结束时间
+ * sumTime：总计用时
  */
-var ArrA = [], ArrB = [],
-    clickIndex,
-    clickN = 0;
+var clickIndex,
+    clickN = 0,
+    atuoTime,
+    errorNum = 0,
+    maxNum,
+    sysTimeN,
+    sysTimeO,
+    sumTime = 0;
+
 var sourceA = [],
     sourceB = [],
     sourceC = [],
@@ -39,51 +51,241 @@ function _event() {
 
         _setListTest()
 
+        _time(20, function () {
+
+            $('#screen3').show()
+            $('#listBox').hide()
+            $('#listBox #list').removeClass().empty()
+
+        })
+
+    })
+    $('#start').click(function () {
+
+        $('#screen3').remove()
+
+        $('#listBox').show()
+
+        $('#listBox #list').removeClass().addClass('list')
+
+        _setList()
+
+        sysTimeN = new Date()
+
+        _time(60, function () {
+
+            sysTimeO = new Date()
+
+            var $cur$Time = (sysTimeO - sysTimeN) / 1000
+
+            sumTime = parseInt(sumTime + $cur$Time)
+
+            maxNum = +($('#target').text()) - 1
+
+            $('#listBox').remove()
+
+            $('#over').show()
+
+            // console.log('maxNum,errorNum,sumTime>>>>>>>>>>>2', maxNum, errorNum, sumTime)
+            _over()
+        })
+
+    })
+    $('#stop').click(function () {
+
+        clearInterval(atuoTime)
+        $('#listBox').hide()
+        $('#stopBox').show()
+
+        sysTimeO = new Date()
+
+        var $cur$Time = (sysTimeO - sysTimeN) / 1000
+
+        sumTime = parseInt(sumTime + $cur$Time)
+
+    })
+    $('#continue').click(function () {
+
+        $('#listBox').show()
+        $('#stopBox').hide()
+        var $time = $('#time').text()
+
+        sysTimeN = new Date()
+
+        _time($time, function () {
+
+            if ($('#screen3').length > 0) {
+
+                $('#screen3').show()
+                $('#listBox').hide()
+                $('#listBox #list').removeClass().empty()
+
+            } else {
+
+                maxNum = +($('#target').text()) - 1
+
+                $('#listBox').remove()
+
+                $('#over').show()
+
+                sysTimeO = new Date()
+
+                var $cur$Time = (sysTimeO - sysTimeN) / 1000
+
+                sumTime = parseInt(sumTime + $cur$Time)
+
+                console.log('maxNum,errorNum,sumTime>>>>>>>>>>>>1', maxNum, errorNum, sumTime)
+                _over()
+
+            }
+
+
+        })
+
     })
 
+    $('.button[data-role="out"]').click(function () {
 
-    $('#list li').on('click', function () {
+        //游戏点击退出按钮
+        _out()
 
-        var dataRole = $(this).attr('data-role'),
+    })
 
-            dataParRole = $(this).parent('ul').attr('data-role'),
+}
 
-            sourceNum = dataRole == 'true' ? 40 : 15
+//游戏结束
+function _over() {
 
-        console.log('sourceNum', sourceNum)
 
-        // 禁止双击
-        if (clickIndex == $(this).index()) return
+    /* ajax 请求接口路径，返回json 数据
+     * maxNum: 游戏总计时间
+     * errorNum ：错误点击次数
+     * */
 
-        if (clickN > 32) return
+    var param = {
 
-        if (dataRole == ArrC[clickN]) {
+        timeObj: sumTime,
 
-            var $val = ArrB[clickN] ? ArrB[clickN] : ''
+        maxNum: maxNum,
 
-            $(this).attr('data-role', $val).children('p').text($val)
+        errorNum: errorNum
 
-            console.log('点击对了', $val)
+    }
 
-            clickIndex = $(this).index()
+    console.log('当前返回参数', param)
 
-            clickN = clickN + 1
+}
+
+//游戏退出
+function _out() {
+
+    console.log('游戏退出')
+
+}
+
+/*** 数字点击事件 ***/
+function _clickEvent(e) {
+
+
+    var dataRole = $(e.target).parent('li').attr('data-role'),
+
+        dataParRole = $(e.target).parents('ul').attr('data-role'),
+
+        sourceNum
+
+
+    // 禁止双击
+    if (clickIndex == $(this).index()) return
+
+    // console.log('clickN,sourceNum', clickN, sourceNum)
+
+    if (dataRole == ArrD[clickN]) {
+
+        var $valNew;
+
+        if (dataParRole != 'true') {
+
+            $valNew = ArrB[clickN] ? ArrB[clickN] : ''
+
+            sourceNum = 15
 
         } else {
 
-            console.log('错误')
+            if (clickN <= 15) {
+
+                $valNew = ArrB[clickN]
+
+            } else {
+
+                $valNew = ArrC[clickN - 16] ? ArrC[clickN - 16] : ''
+            }
+
+            sourceNum = 40
+
         }
 
-    })
+        $(e.target).parent('li').attr('data-role', $valNew).children('p').text($valNew)
+
+        // console.log('点击对了', $valNew)
+
+        clickIndex = $(e.target).parent('li').index()
+
+        clickN = clickN + 1
+
+        $('#target').text(ArrD[clickN])
+
+        if (clickN >= sourceNum) {
+
+            //点击完成
+
+            clearInterval(atuoTime)
+
+            if (dataParRole != 'true') {
+
+                $('#screen3').show()
+
+                $('#listBox').hide()
+
+                $('#listBox #list').removeClass().empty()
+
+            } else {
+
+                sysTimeO = new Date()
+
+                var $cur$Time = (sysTimeO - sysTimeN) / 1000
+
+                sumTime = parseInt(sumTime + $cur$Time)
+
+                maxNum = ArrD[39]
+
+                // console.log('maxNum,errorNum,sumTime>>>>>>>>3', maxNum, errorNum, sumTime)
+
+                _over()
+                $('#listBox').remove()
+
+                $('#over').show()
+
+            }
+
+            return
+
+        }
+
+
+    } else {
+
+        if (dataParRole == 'true') {
+
+            errorNum = errorNum + 1
+
+        }
+        console.log('错误')
+    }
 
 
 }
 
-
-/*** 设置列表
- * arr：数组
- * num：随机个数
- ***/
+/*** 设置列表正式测验 ***/
 function _setList() {
 
 //source 资源库
@@ -92,10 +294,15 @@ function _setList() {
     sourceC = [33, 34, 35, 36, 37, 38, 39, 40];
     ArrA = _getArrayItems(sourceA, 16);
     ArrB = _getArrayItems(sourceB, 16);
-    ArrC = _getArrayItems(sourceB, 8);
+    ArrC = _getArrayItems(sourceC, 8);
     ArrD = sourceA.concat(sourceB).concat(sourceC);
+
+    clickIndex = ''
+
+    clickN = 0;
+
     for (var i = 0; i < 16; i++) {
-        $('#list').append('<li  data-role="' + ArrA[i] + '"><p>' + ArrA[i] + '</p></li>')
+        $('#list').append('<li  onclick="_clickEvent(event)"  data-role="' + ArrA[i] + '"><p>' + ArrA[i] + '</p></li>')
     }
 
     $('#list').attr('data-role', 'true')
@@ -115,6 +322,7 @@ function _setList() {
 
 }
 
+/*** 设置列表练习题 ***/
 function _setListTest() {
 
     //source 资源库
@@ -125,7 +333,7 @@ function _setListTest() {
     ArrD = sourceA.concat(sourceB);
 
     for (var i = 0; i < 9; i++) {
-        $('#list').append('<li  data-role="' + ArrA[i] + '"><p>' + ArrA[i] + '</p></li>')
+        $('#list').append('<li onclick="_clickEvent(event)"  data-role="' + ArrA[i] + '"><p>' + ArrA[i] + '</p></li>')
     }
 
     $('#list').attr('data-role', 'false')
@@ -145,6 +353,31 @@ function _setListTest() {
 
 }
 
+/*** 倒计时
+ * i：时间
+ * fn：倒计时结束回调
+ ***/
+function _time(i, fn) {
+
+    var timeFn = function () {
+
+        i = i - 1
+
+        $('#time').text(i)
+        if (i == 0) {
+
+            clearInterval(atuoTime)
+
+            fn && fn.call(this)
+
+        }
+
+    }
+
+    atuoTime = setInterval(timeFn, 1000);
+
+
+}
 
 /*** 数组随机
  * arr：数组
@@ -176,30 +409,4 @@ function _getArrayItems(arr, num) {
         }
     }
     return return_array;
-}
-
-/*** 数组去重
- * sumArr：大数组
- * subArr：去重元素数组
- ***/
-function _repeat(sumArr, subArr) {
-
-    var repArr = [];
-
-    for (var i = 0; i < sumArr.length; i++) {
-
-        for (var n = 0; n < subArr.length; n++) {
-
-            if (sumArr[i] == subArr[n]) {
-
-                sumArr.splice(i, 1);
-
-                repArr = sumArr
-
-            }
-
-        }
-
-    }
-    return repArr;
 }
